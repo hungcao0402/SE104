e4bd8584dc751baeb1175eec01e7c974418c969f
TomatoFT <thomas.lezard.2002@gmail,com>
2022-06-06 22:55:49 +0700

    The last update

--- gara_management/settings.py
+++ gara_management/settings.py
@@ -129,3 +129,6 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
